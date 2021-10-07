104d7753221a8b041fe8ec84ecf0ca9aa97ced88
Cristian Cantero <cdc831@gmail.com>
2021-10-07 14:21:29 -0300

    Agrego api basico

---
+++ src/main/java/com/example/demo/controller/indexApiController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class indexApiController {
+    
+    @Value("${prop.valor}")
+    private String nombre;
+
+    @GetMapping("/")
+    public String index(){
+        return "Hola " + nombre;
+    }
+}
--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class indexController {
 
+    @Value("${prop.valor}")
+    private String nombre;
+
     @GetMapping({"/index", "/", "/home"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("Nombre", nombre);
         return "index";
     }
 }
